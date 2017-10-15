3b76239cf45538b7c5beade6cd24105e541a47e3
Robin Watson <drumsrobin@gmail.com>
2017-10-15 16:50:34 -0400

    initial js file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import './index.css';
+let userName = 'Jamie';
+let knowsJavaScript = true;
 
-document.getElementById('root').innerHTML = 'Hello World';
-
-alert('Hello World!');
+if (knowsJavaScript && userName) {
+  console.log('Great, ' + userName + '! Get ready to practice your JavaScript!');
+} else if (knowsJavaScript) {
+  console.log('Great! Get ready to practice your JavaScript!');
+} else if (userName) {
+  console.log('Great, ' + userName + '! Get ready to learn something new!');
+} else {
+  console.log('Great! Get ready to learn something new!');
+}
