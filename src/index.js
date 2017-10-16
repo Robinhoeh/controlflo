d3ae69e75666e47708b34564fd15ef8ebe021093
Robin Watson <drumsrobin@gmail.com>
2017-10-16 13:26:22 -0400

    logical operators

--- src/index.js
+++ src/index.js
@@ -111,6 +111,23 @@ if  (moonPhase === 'full') {
     console.log('Invalid moon phase');
 }
 
+//============Logical Operators
+
+let moonPhase = 'full';
+let isFoggyNight = false;
+if  (moonPhase === 'full' || isFoggyNight === true) {
+  console.log('Howl!');
+  
+} else if (moonPhase === 'mostly full') {
+  console.log('Arms and legs are getting hairier');
+  
+} else if (moonPhase === 'mostly new') {
+  console.log('Back on two feet');
+  
+} else {
+  console.log('Invalid moon phase');
+}
+
 
 
 
