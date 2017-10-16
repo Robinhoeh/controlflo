67c35e39e05dd08b48398e426c25467dd0cb578c
Robin Watson <drumsrobin@gmail.com>
2017-10-16 13:23:19 -0400

    more comparsions

--- src/index.js
+++ src/index.js
@@ -86,6 +86,32 @@ if (hungerLevel > 7) {
   console.log('We can eat later');
 }
 
+//===========more comparison
+
+let moonPhase = 'full';
+if  ( moonPhase === 'full') {
+  console.log('Howl!');
+} else {
+  console.log('I swear I am not a werewolf');
+}
+
+//==============
+
+let moonPhase = 'solar eclipse';
+if  (moonPhase === 'full') {
+  console.log('Howl!');
+  
+} else if (moonPhase === 'mostly full') {
+    console.log('Arms and legs are getting hairier');
+  
+} else if (moonPhase === 'mostly new') {
+    console.log('Back on two feet');
+  
+} else {
+    console.log('Invalid moon phase');
+}
+
+
 
 
 
