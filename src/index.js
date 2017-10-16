c39e46b62e8db667eb92d7d3fef94e60ef07d89e
Robin Watson <drumsrobin@gmail.com>
2017-10-16 13:07:40 -0400

    comparison example

--- src/index.js
+++ src/index.js
@@ -77,6 +77,14 @@ if (!favoritePhrase) {//! flips condition to read falsey
 }
 
 
+//==============Comparison Operators
+
+let hungerLevel = 10;
+if (hungerLevel > 7) {
+  console.log('Time to eat!')
+} else {
+  console.log('We can eat later');
+}
 
 
 
