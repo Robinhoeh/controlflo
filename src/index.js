c2f38417d639ce5ac3a9529231139d148c88c095
Robin Watson <drumsrobin@gmail.com>
2017-10-16 13:52:42 -0400

    eightball program

--- src/index.js
+++ src/index.js
@@ -146,6 +146,12 @@ let favoritePhrase = 'Love That!';
 favoritePhrase === 'Love That!' ? console.log('I love that!') : console.log("I don't love that!");
 
 
+//==========Eight Ball
+
+let userName = '';
+userName !== '' ? console.log(`Hello ${userName}`)
+: console.log('Hello!');
+
 
 
 
